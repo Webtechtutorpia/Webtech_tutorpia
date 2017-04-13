54e2bf9880108e9fd579d9ed9feff9011276f44e
Isabel Bayer <Isabel@MacBookPro.local>
2017-04-13 08:16:08 +0200

    Reiter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,142 +11,42 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            body {
-                font-family: sans-serif;
-                color: #333333;
-                padding: 8em 0 3em;
-            }
-
-            body,
-            .wrapper {
-                margin: 10px auto;
-                max-width: 60em;
-            }
-            footer {
-                position: fixed;
-                padding: 10px;
-                bottom: 0;
-                left: 0;
-                right: 0;
-                background: #c32e04;
-                border-color: #8a9da8;
-            }
-            footer p {
-                float: right;
-                margin: 0;
-            }
-
-            #navlist li {
-                display: inline;
-                list-style-type: none;
-                padding-right: 20px;
-            }
-            header {
-                position: fixed;
-                top: 0px;
-                left: 0px;
-                right: 0px;
-                text-align: center;
-                padding: 10px;
-                background: #F1F3F4;
-                border-bottom: 1px solid #d5d5d5;
-            }
-            header img {
-                width: 13em;
-                margin-right: 50px;
-                float: left;
-            }
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
 
 
-        </style>
     </head>
     <body>
     <header>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
 
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-        @endif
-        </div>
+        <nav>
+            <ul class="nav nav-tabs">
+                @if (Auth::check())
+                    <li role="presentation" class="active"><a href="#">Übersicht</a></li>
+                    <li role="presentation"><a href="#">Kurse</a></li>
+                    <li role="presentation"><a href="#">Abgaben</a></li>
+
+                @else
+                    <ul class="nav navbar-nav navbar-right">
+                        <li role="presentation" class="bottom-right"><a href="{{ url('/login') }}">Login</a></li>
+                        <li role="presentation" class="right"><a href="{{ url('/register') }}">Registrierung</a></li>
+                    </ul>
+            </ul>
+            @endif
+        </nav>
+
     </header>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-                <div class="title m-b-md">
-                    Tutorpia
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <div class="row">
+        <div class="col-xs-6 col-md-6 bild">
+            <a href="#" class="thumbnail">
+                <img src="http://placehold.it/500x300">
+            </a>
         </div>
     </div>
         <footer>
-            <div class="r4">
+            <div>
                 <ul id="navlist">
                     <li class="first">Datenschutz</li>
                     <li>Impressum</li>
