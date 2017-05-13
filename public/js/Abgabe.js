8485933ab4aae22cd0a409b8c2bf302a8e0e7af8
Jannis Grebenstein <Jannis Grebenstein>
2017-05-13 17:28:35 +0200

    Ajax Tabellenanbindung

--- public/js/Abgabe.js
+++ public/js/Abgabe.js
@@ -1,18 +1,47 @@
-
-
+$( document ).ready(function() {
+    console.log( "ready!" );
+    search('');
+});
     function search(name) {
-
-
     //nachfragen delay und Buchstabe zuwenig Problem
         console.log(name);
+        remove();
         $('#ausgabe').html("");
+       var x= jQuery('<tbody></tbody>');
         $.getJSON( "/json?tfsearch="+name, function(data) {
             $.each(data, function(index,user) {
-                var link = "<p>"+user.name +"</p>";
-                $(link).appendTo("#ausgabe");
+               // var link = "<p>"+user.name +"</p>";
+                $(x).append('<tr><td>'+ user.name + '</td></tr>');
+                $(x).appendTo('#tabelle');
+              //  $(link).appendTo("#ausgabe");
             });
-        })
+        });
     };
 
 
+    // ungenutzt
+// function add(){
+//
+//     jQuery('<tbody/>').append(jQuery('<tr/>')).append(jQuery('<td>hallo text </td>')).appendTo('#tabelle');
+//     //jQuery('<div/> Ich bin ein text </div>',{ id:"output"}).appendTo('#tabelle');
+//    // $('#tabelle').append(jQuery('<div> Hallo du </div>'));
+//     console.log('hier');
+//   $('#output').append().query('<p/>').append('Ich bin ein Text');
+//   $('#tabelle').append('#output');
+//     //    var anfang = '<tbody><tr>';
+//     // var td1='<td/>';
+//     // console.log("21");
+//     // $(td1).html('Beispiel');
+//     // var ende= '</tr></tbody>';
+//     // console.log("24");
+//     // $('#tabelle').append(anfang).append(td1).append(ende);
+//
+//     $('')
+// }
+
+function remove(){
+    var x =$('#tabelle').find('tbody').remove();
+    console.log('gelöscht');
+
+}
 
--- resources/views/Tutor/abgabe.blade.php
+++ resources/views/Tutor/abgabe.blade.php
@@ -1,84 +1,98 @@
 @extends('layouts.app')
 
 @section('content')
+    {{--Problem jquery aus layouts lädt nicht schnell genug--}}
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="{{ URL::asset('js/Abgabe.js') }}"></script>
     @if (Auth::user()->rolle=="Tutor" || Auth::user()->rolle=="Professor" )
-    <div class="container">
-        <div class="row">
-        <h2>Tutorenmodus: ALDA</h2>
+        <div class="container">
+            <div class="row">
+                <h2>Tutorenmodus: ALDA</h2>
 
-            <div class="col-md-4 col-md-offset-8" id="anhang">
-            <div class="input-group">
-            <form method="get" action="/search">
-                <input type="text" class="form-control" placeholder="Suche nach..." name="tfsearch"  onkeypress="search(this.value)">
-                <span class="input-group-btn">
-             <button class="btn btn-default" type="submit" ><span class="glyphicon glyphicon-search" aria-hidden="true" ></span></button></span>
-            </form>
+                <div class="col-md-4 col-md-offset-8" id="anhang">
+                    <div class="input-group">
+                        <form method="get" action="/search">
+                            <input type="text" class="form-control" placeholder="Suche nach..." name="tfsearch"
+                                   onkeypress="search(this.value)">
+                            <span class="input-group-btn">
+             <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"
+                                                                 aria-hidden="true"></span></button></span>
+                        </form>
+                    </div>
+                </div>
+                <button onclick="add()"> add</button>
+                <button onclick="remove()">remove</button>
+                {{--<div class="table-responsive">--}}
+                <table class="table table-responsive" id="tabelle">
+                    <thead>
+                    <tr>
+                        <th> Name</th>
+                    </tr>
+                    </thead>
+                </table>
+
+                {{--<table class="table">--}}
+                {{--<thead>--}}
+                {{--<tr>--}}
+                {{--<th class="col-md-3 col-xs-3">Vorname</th>--}}
+                {{--<th class="col-md-3 col-xs-3">Nachname</th>--}}
+                {{--<th class="col-md-1 col-xs-1">Aufgabe1</th>--}}
+                {{--<th class="col-md-1 col-xs-3">Aufgabe2</th>--}}
+                {{--<th class="col-md-1 col-xs-3">Aufgabe3</th>--}}
+                {{--<th class="col-md-1 col-xs-3">Aufgabe4</th>--}}
+                {{--<th class="col-md-1 col-xs-3">Aufgabe5</th>--}}
+                {{--<th class="col-md-1 col-xs-3">Aufgabe6</th>--}}
+
+                {{--</tr>--}}
+                {{--</thead>--}}
+                {{--<tbody>--}}
+                {{--<tr>--}}
+                {{--<td>John</td>--}}
+                {{--<td>Doe</td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--</tr>--}}
+                {{--<tr>--}}
+                {{--<td>Mary</td>--}}
+                {{--<td>Moe</td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--</tr>--}}
+                {{--<tr>--}}
+                {{--<td>July</td>--}}
+                {{--<td>Dooley</td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+
+                {{--</tr>--}}
+                {{--<tr>--}}
+                {{--<td>Klaus</td>--}}
+                {{--<td>Peter</td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>--}}
+                {{--<td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>--}}
+                {{--</tr>--}}
+                {{--</tbody>--}}
+                {{--</table>--}}
+
+                <div id="ausgabe">hallo</div>
             </div>
-            </div>
-        {{--<div class="table-responsive">--}}
-         <table class="table">
-            <thead>
-            <tr>
-                <th class="col-md-3 col-xs-3">Vorname</th>
-                <th class="col-md-3 col-xs-3">Nachname</th>
-                <th class="col-md-1 col-xs-1">Aufgabe1</th>
-                <th class="col-md-1 col-xs-3">Aufgabe2</th>
-                <th class="col-md-1 col-xs-3">Aufgabe3</th>
-                <th class="col-md-1 col-xs-3">Aufgabe4</th>
-                <th class="col-md-1 col-xs-3">Aufgabe5</th>
-                <th class="col-md-1 col-xs-3">Aufgabe6</th>
-
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>John</td>
-                <td>Doe</td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-            </tr>
-            <tr>
-                <td>Mary</td>
-                <td>Moe</td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-            </tr>
-            <tr>
-                <td>July</td>
-                <td>Dooley</td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-
-            </tr>
-            <tr>
-                <td>Klaus</td>
-                <td>Peter</td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-minus btn-warning"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-remove btn-danger"></a></td>
-                <td class="text-center"><a href="{{ url('/aufgabe_example') }}" class="glyphicon glyphicon-ok btn-success"></a></td>
-            </tr>
-            </tbody>
-        </table>
-
-          
-    </div>
-    </div>
+        </div>
 
     @endif
-    @endsection
+@endsection
